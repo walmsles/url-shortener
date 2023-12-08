7cfa01d072091fe650e2948546aca98bb96fbbac
walmsles <2704782+walmsles@users.noreply.github.com>
2023-12-08 11:34:20 +1100

    minor final updates

--- scripts/make-deps.sh
+++ scripts/make-deps.sh
@@ -4,11 +4,24 @@ for folder in $(ls -d ${WORKDIR:-${PWD}}/services/* | grep -v __pycache__)
 do
     if [[ -d ${folder} ]]; then
         group=$(basename ${folder})
-        if [[ $(poetry show --with=${group} --without=dev) ]]; then
+
+        # check if service group exists
+        poetry show --with=${group} --quiet
+        group_exists=$?
+        all_groups=""
+        if [[ $group_exists -eq 0 ]]; then
+            # export main dependencies AND specific group dependencies
             echo creating requirements in ${folder}
-            poetry export --without-hashes --with=${group} > ${folder}/requirements.txt
+            all_groups="${all_groups} --with=${group}"
+            poetry export --without-hashes --with=${group} --with main > ${folder}/requirements.txt
+        else
+            # export main only (to avoid poetry errors)
+            echo creating requirements in ${folder}
+            poetry export --without-hashes --with main > ${folder}/requirements.txt
         fi
     fi
 done
 
-poetry export --without-hashes --with dev > ${WORKDIR:-${PWD}}/tests/requirements.txt
+# export
+echo "poetry export --without-hashes --with dev $all_groups > ${WORKDIR:-${PWD}}/tests/requirements.txt"
+poetry export --without-hashes --with dev $all_groups > ${WORKDIR:-${PWD}}/tests/requirements.txt
